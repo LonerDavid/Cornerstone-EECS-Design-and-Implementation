32a0eb85c87fbcd2078b12876790f876f231836b
LonerDavid <105446636+LonerDavid@users.noreply.github.com>
2024-04-20 20:45:30 +0800

    Update node.h

--- midterm-project/arduino/midterm_project/node.h
+++ midterm-project/arduino/midterm_project/node.h
@@ -12,6 +12,8 @@ int extern _Tp;
 
 // TODO: add some function to control your car when encounter a node
 // here are something you can try: left_turn, right_turn... etc.
+double turn = 30;
+bool in_node = false;
 
 void action(int cmd){
     switch (cmd)
@@ -42,25 +44,76 @@ void action(int cmd){
 }
 
 void car_front(){
-
+    MotorWriting(_Tp,_Tp);
+    delay(2000);
 }
 
 void car_back(){
-
+    MotorWriting(_Tp,-_Tp);
+    delay(2500);
+    MotorWriting(_Tp, _Tp);
+    delay(800);
+    in_node = car_in_node();
+    while (in_node){
+        MotorWriting(_Tp, -_Tp);
+        delay(10);
+        in_node = car_in_node();
+    }
+    MotorWriting(0, 0);
+    delay(200);
 }
 
 void car_right(){
-
+    double vR = _Tp - turn;
+    double vL = _Tp + turn;
+    MotorWriting(vL,vR);
+    delay(800);
+    in_node = car_in_node();
+    while(in_node){
+        MotorWriting(vL,vR);  
+        delay(10);
+        in_node = car_in_node();
+    }
+    MotorWriting(0, 0);
+    delay(200);
 }
 
 void car_left(){
-
+    double vR = _Tp + turn;
+    double vL = _Tp - turn;
+    MotorWriting(vL,vR);
+    delay(800);
+    in_node = car_in_node();
+    while(in_node){
+        MotorWriting(vL,vR);  
+        delay(10);
+        in_node = car_in_node();
+    }
+    MotorWriting(0, 0);
+    delay(200);
 }
 
 void car_start(){
-
+    MotorWriting(_Tp,_Tp);
+    delay(1000);
 }
 
 void car_end(){
-    
+    MotorWriting(0,0);
+    delay(1000);
+}
+
+bool car_in_node(){
+    int ll = digitalRead(IRpin_LL);
+    int l = digitalRead(IRpin_L);
+    int m = digitalRead(IRpin_M);
+    int r = digitalRead(IRpin_R);
+    int rr = digitalRead(IRpin_RR);
+
+    if (ll+l+m+r+rr == 5) {
+        return true;
+        }
+    else {
+        return false;
+        }
 }
