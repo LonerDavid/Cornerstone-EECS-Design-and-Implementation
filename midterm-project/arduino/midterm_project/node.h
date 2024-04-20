19f9e3e5c86522812f25b0f4f79219ae2175bab8
LonerDavid <105446636+LonerDavid@users.noreply.github.com>
2024-04-20 10:46:48 +0800

    Update node.h

--- midterm-project/arduino/midterm_project/node.h
+++ midterm-project/arduino/midterm_project/node.h
@@ -12,3 +12,55 @@ int extern _Tp;
 
 // TODO: add some function to control your car when encounter a node
 // here are something you can try: left_turn, right_turn... etc.
+
+void action(int cmd){
+    switch (cmd)
+    {
+    case 0:
+        break;
+    case 1:  //Front
+        car_front();
+        break;
+    case 2:  //Back
+        car_back();
+        break;
+    case 3:  //Right
+        car_right();
+        break;
+    case 4:  //Left
+        car_left();
+        break;
+    case 5:  //Start
+        car_start();
+        break;
+    case 6:  //End
+        car_end();
+        break;
+    default:
+        break;
+    }
+}
+
+void car_front(){
+
+}
+
+void car_back(){
+
+}
+
+void car_right(){
+
+}
+
+void car_left(){
+
+}
+
+void car_start(){
+
+}
+
+void car_end(){
+    
+}
